f8ac4e66b69c34ee2fa6ae199a9ccf99b0ca80bb
alexjose131 <ajfernandez.17@est.ucab.edu.ve>
2020-11-17 15:39:52 -0400

    commit para recibir cambios

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
-from modules.constants import DRINKS, INGREDIENTS, PIZZAS, DESSERTS
+import sys
+from modules.constants import WELCOME, OPTION_1, OPTION_2, OPTION_3, OPTION_4, FAREWELL, PIZZAS
 
-print(DRINKS)
+def header():
+  print(20*'*')
+  print("* PIZZERIA UCAB *")
+  print(20*'*')
+
+def menu():
+  print()
+  print(WELCOME)
+  print()
+  print(OPTION_1)
+  print(OPTION_2)
+  print(OPTION_3)
+  print(OPTION_4)
+  print()
+  option = int(input("Seleccione una opción: "))
+  print()
+  if option == 1:
+    # LLAMAR A FUNCION DE PIZZERIA
+    pass
+  elif option == 2:
+    # LLAMAR A CONSULTA DE COMPRAS
+    pass
+  elif option == 3:
+    # LLAMAR A LIMPIEZA DE REGISTRO
+    pass
+  elif option == 4:
+    print(FAREWELL)
+    sys.exit()
+    
+  print(option)
+
+header()
+menu()
